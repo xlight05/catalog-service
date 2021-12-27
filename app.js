98d40627e0f7c96bb20a0ee7832c167970ac762a
Anjana Supun <anjanas@wso2.com>
2021-12-27 08:18:01 +0530

    Change service path

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use('/catalog', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -28,7 +28,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({"message":"an error occured"});
 });
 
 module.exports = app;
