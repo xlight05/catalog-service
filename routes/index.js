9dbd0689362efde57cc947741eecc053bde251d7
Anjana Supun <anjanas@wso2.com>
2022-01-04 21:19:18 +0530

    Add dummydata to catalog

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const dummyBooks = [
+  {
+    id: "1",
+    bookName: "HarryPotter1",
+    author: "J.K.Rowling",
+    price: "500"
+  }, {
+    id: "2",
+    bookName: "HarryPotter2",
+    author: "J.K.Rowling",
+    price: "800"
+  }, {
+    id: "3",
+    bookName: "HarryPotter3",
+    author: "J.K.Rowling",
+    price: "900"
+  }
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // Call payment service
-  //URL - Read from env. 
-  
-  res.json({"message": "Hello from catalog service"});
+router.get('/', function (req, res, next) {
+  res.json(dummyBooks);
 });
 
 module.exports = router;
